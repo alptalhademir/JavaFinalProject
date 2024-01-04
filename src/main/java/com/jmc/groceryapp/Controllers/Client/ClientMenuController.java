ba37ab4fac02e0efd875f9efeb9eb82d3c47e87d
edinkoc <edin.koc@stu.khas.edu.tr>
2024-01-04 23:58:56 +0300

    first push

--- src/main/java/com/jmc/groceryapp/Controllers/Client/ClientMenuController.java
+++ src/main/java/com/jmc/groceryapp/Controllers/Client/ClientMenuController.java
@@ -1,4 +1,22 @@
 package com.jmc.groceryapp.Controllers.Client;
 
-public class ClientMenuController {
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientMenuController implements Initializable {
+    public Button home_btn;
+    public Button chart_btn;
+    public Button orders_btn;
+    public Button account_btn;
+    public Button logout_btn;
+    public Button aboutus_btn;
+    public Button contact_btn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
