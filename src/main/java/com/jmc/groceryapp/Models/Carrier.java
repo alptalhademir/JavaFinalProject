7e40d8cd0218bb6c49895a572d63bd85b98ac445
zeyneperkilic <88237258+zeyneperkilic@users.noreply.github.com>
2024-01-10 18:52:18 +0300

    property changes on models

--- src/main/java/com/jmc/groceryapp/Models/Carrier.java
+++ src/main/java/com/jmc/groceryapp/Models/Carrier.java
@@ -1,33 +1,33 @@
 package com.jmc.groceryapp.Models;
 
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+import javafx.beans.property.*;
 
 import java.time.LocalDate;
 
 public class Carrier extends User {
-    private int carrierID;
-    private String phoneNumber;
-    private LocalDate creationDate;
+    private final IntegerProperty carrierID = new SimpleIntegerProperty();
+    private final StringProperty phoneNumber = new SimpleStringProperty();
+    private final ObjectProperty<LocalDate> creationDate = new SimpleObjectProperty<>();
 
     public Carrier(String firstName, String lastName, String userName, String password, String userRole,
-                   String phoneNumber, LocalDate creationDate){
+                   String phoneNumber, LocalDate creationDate,Integer carrierID){
         super(firstName, lastName, userName, password, userRole);
 
-        this.phoneNumber = phoneNumber;
-        this.creationDate = creationDate;
+
+        this.phoneNumber.set(phoneNumber);
+        this.creationDate.set(creationDate);
+        this.carrierID.set(carrierID);
+
 
 
     }
 
-    public int getCarrierID() {return carrierID;}
+    public IntegerProperty getCarrierID() {return carrierID;}
 
-    public void setCarrierID(int carrierID) {this.carrierID = carrierID;}
+    public void setCarrierID(int carrierID) {this.carrierID.set(carrierID);}
 
-    public String getPhoneNumber() {return phoneNumber;}
-    public LocalDate getCreationDate() {return creationDate;}
+    public StringProperty getPhoneNumber() {return phoneNumber;}
+    public ObjectProperty<LocalDate> getCreationDate() {return creationDate;}
 
-    public void setCreationDate(LocalDate creationDate) {this.creationDate = creationDate;}
+    public void setCreationDate(LocalDate creationDate) {this.creationDate.set(creationDate);}
 }
